d7dc133407a8f47b150de84ea03eff8148769a9a
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-11-09 00:32:38 +0000

    front set up done

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,14 +1,17 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
-
+	console.log(store.token)
 	return (
 		<div className="text-center mt-5">
-			<link to={signup} className="btn btn-primary">
+			<Link to={"/signup"} className="btn btn-primary">Signup</Link>
+			<Link to={"/login"} className="btn btn-primary">Login</Link>			
+			<Link to={"/profile"} className="btn btn-primary">Profile</Link>
 		</div>
 	);
 };
